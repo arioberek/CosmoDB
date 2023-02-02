af022ea0eba462e138d9ed44259797fba1fdd62f
arrbrk1 <acorelado@gmail.com>
2023-02-02 19:37:04 -0300

    initial setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Center, Input, NativeBaseProvider } from "native-base";
+import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} maxW="80%" mx="auto">
+        <Input variant="filled" placeholder="Type Your Query" />
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Input.tsx
@@ -0,0 +1,9 @@
+import { Box } from "native-base";
+
+export const Input = () => {
+  return (
+    <Box alignItems="center">
+      <Input mx="3" placeholder="Input" w="100%" />
+    </Box>
+  );
+};
